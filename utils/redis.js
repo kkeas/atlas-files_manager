a8bc629b01c70e7d108446e8aa4bf2196a01633a
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-03-18 12:49:50 -0500

    first commit

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+
+const redis = require('redis')
+const { promisify } = require('util');
+
+class RedisClient {
+
+    constructor() {
+        this.client = redis.createClient();
+        this.getAsync = promisify(this.client.get).bind(this.client);
+        this.setAsync = promisify(this.client.set).bind(this.client);
+        this.delAsync = promisify(this.client.del).bind(this.client);
+        this.client.on('error', (err) => {
+          console.log(err);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async set(key, value, duration) {
+        return this.client.set(key, value, 'EX',  duration)
+    }
+
+    async get(key) {
+        return this.getAsync(key);
+      }
+}
+
+
+//export 
+const redisClient = new RedisClient()
+module.exports = redisClient
