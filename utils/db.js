d9e99fc3a07d207f0c341b7416fb0d5962cd4b89
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-03-18 13:50:42 -0500

    mongo connect

---
+++ utils/db.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    this.MongoClient = new MongoClient(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+        });
+    }
+
+    isAlive() {
+        return this.MongoClient.isConnected();
+    }
+
+    async nbUsers() {
+        return await this.users.countDocuments({});
+    }
+
+    async nbFiles() {
+        return await this.nbFiles.countDocuments({});
+    }
+}
+
+//export
+const dbClient = new DBClient();
+module.exports = dbClient;
